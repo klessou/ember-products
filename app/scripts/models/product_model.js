5c45f6b6a72af2d06bf9cdddcdc63fe0572d01aa
David Piry <davidpiry@gmail.com>
2014-09-03 22:15:51 +0200

    Better fixtures.

--- app/scripts/models/product_model.js
+++ app/scripts/models/product_model.js
@@ -21,18 +21,26 @@ EmberProducts.Product.FIXTURES = [
   {
     id: 0,
     
-    name: 'foo',
+    name: 'Filson 256',
     
-    description: 'foo'
+    description: 'Our toughest, most dependable briefcase is appropriate for everyday use in the field. \
+      Inside is a wide carrying compartment, two full-length open pockets and sewn-on compartments for calculator, \
+      business cards, pencils and pens. Narrow pockets on both ends and full-length outer pockets on both sides. \
+      You won\'t find more durability in a fabric briefcase. Meets carry-on requirements. Made in USA.'
     
   },
   
   {
     id: 1,
     
-    name: 'foo',
+    name: 'Filson 257',
     
-    description: 'foo'
+    description: 'Protect your laptop in this large briefcase made with our nearly indestructible Rugged Twill. \
+      Inside are four roomy full-length sections. One is the perfect size for a laptop computer. \
+      More pockets hold a battery pack, diskettes, calculator, business cards, pens, pencils and notebooks. \
+      Narrow pockets on both ends, full-length outer pockets on both sides and adjustable, removable leather \
+      shoulder strap with brass-plated roller buckle and leather pad. Leather grip and storm flap with snap closures. \
+      Double bottom. Meets carry-on requirements. Made in USA.'
     
   }
   
