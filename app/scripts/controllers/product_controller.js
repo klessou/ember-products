b8506aaf6ddf4e44e47b0108cd8c8145f3936d1e
David Piry <davidpiry@gmail.com>
2014-09-09 20:56:29 +0200

    New delete action.

--- app/scripts/controllers/product_controller.js
+++ app/scripts/controllers/product_controller.js
@@ -1,4 +1,22 @@
 EmberProducts.ProductController = Ember.ObjectController.extend({
-  // Implement your controller here.
+  editMode: false,
+
+	actions: {
+		delete: function(){
+			this.toggleProperty('deleteMode');
+		},
+		cancelDelete: function(){
+			this.set('deleteMode', false);
+		},
+		confirmDelete: function(){
+			// delete a user
+			this.get('model').deleteRecord();
+			this.get('model').save();
+			// then transition to the UsersRoute
+			this.transitionToRoute('products');
+			// set deleteMode back to false
+			this.set('deleteMode', false);
+		}
+},
 });
 
