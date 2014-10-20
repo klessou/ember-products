8ec2d1745c7d7a83601f877a55df4f80e0cb23f8
David Piry <davidpiry@gmail.com>
2014-10-20 22:27:26 +0200

    Autocomplete brand field.

--- app/scripts/components/typeahead_brands.js
+++ app/scripts/components/typeahead_brands.js
@@ -2,36 +2,43 @@ Ember.TypeAheadBrandsComponent = Ember.Component.extend({
       
   didInsertElement: function() {
     this._super();
-    var self = this;
-    
-    if(!this.get("data")){
-      throw "No data property set";
-    }
+    var brands = []; // brands list. 
 
-    // constructs the suggestion engine
-    var brandList = new Bloodhound({
-      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-      queryTokenizer: Bloodhound.tokenizers.whitespace,
-      
-      local: $.map(self.get("data"), function(brand) { return { value: brand }; })
+    var brandsPromise = this.get('data');
+
+    // Populates brands list.
+    brandsPromise.then(function(results) {
+      results.forEach(function(brand, index) {
+        brands.push(brand.get('name'));
+      });
     });
 
-    // kicks off the loading/processing of `local` and `prefetch`
-    brandList.initialize();
-     
-    $('.brandList .typeahead').typeahead({
-      hint: true,
-      highlight: true,
-      minLength: 1
-    },
-    {
-      name: 'brands',
-      displayKey: 'value',
-      // `ttAdapter` wraps the suggestion engine in an adapter that
-      // is compatible with the typeahead jQuery plugin
-      source: brandList.ttAdapter()
-    });
+    // Constructs autocomplete when brands list is entierely populated.
+    brandsPromise.finally(function() {
+      // constructs the suggestion engine
+      var brandList = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        
+        local: $.map(brands, function(brand) { return { value: brand }; })
+      });
 
+      // kicks off the loading/processing of `local` and `prefetch`
+      brandList.initialize();
+       
+      $('.brandList .typeahead').typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 1
+      },
+      {
+        name: 'brands',
+        displayKey: 'value',
+        // `ttAdapter` wraps the suggestion engine in an adapter that
+        // is compatible with the typeahead jQuery plugin
+        source: brandList.ttAdapter()
+      });
+    });
   },
 
 });
